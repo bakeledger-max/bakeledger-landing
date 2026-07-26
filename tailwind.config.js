888df03a5a236f6589fd4da47eae46adce25305c
bakeledger-max <bakeledger@gmail.com>
2026-07-26 11:47:35 +0530

    tailwind css zipping

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#25D366',
+        dark: '#111827',
+        light: '#F3F4F6'
+      }
+    }
+  },
+  plugins: []
+}
